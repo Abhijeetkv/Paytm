dc2b70f7c59182d28366dede28fd1860b7316b3a
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-08-09 20:14:46 +0530

    feat: update package.json to add module type and new scripts
    - Added "type": "module" to enable ES module syntax.
    - Introduced "start" and "dev" scripts for easier application management.
    - Added new dependencies: cors, dotenv, and nodemon for enhanced functionality.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './db/db.js';
+
+const app = express();
+
+await connectDB()
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Paytm Backend!');
+});
 
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
